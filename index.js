1936253e0bf22e0014bb010132463d46bb9c9bcc
sachin jat <53407215+sachinjat2802@users.noreply.github.com>
2022-11-13 10:35:05 +0000

    aded questions

---
+++ index.js
@@ -0,0 +1,74 @@
+//1.get users from array of user object 
+// const users = [
+// 	{
+// 		name: "sachin",
+// 		id: 1,
+// 		isActive: true
+// 	},
+// 	{
+// 		name: "siya",
+// 		id: 1,
+// 		isActive: true
+// 	},
+// 	{
+// 		name: "hansikha",
+// 		id: 1,
+// 		isActive: false
+// 	}
+// ]
+
+// const userNames = users.map((user) => user.name)
+
+// console.log(userNames)
+
+//2.How to define a class with properties and methods in JavaScript?
+// class Student{
+// 	name;
+// 	clas;
+
+// 	constructor(name,clas){
+// 		this.name=name
+// 		this.clas=clas
+// 	};
+// 	data(){
+//          console.log(this.name,this.clas)
+// 	};
+// }
+
+// const s1 = new Student("sachin",12)
+// s1.data()
+
+//3.How to implement class inheritance in JavaScript?
+
+// class Student {
+// 	name;
+// 	clas;
+
+// 	constructor(name, clas) {
+// 		this.name = name
+// 		this.clas = clas
+// 	};
+// 	data() {
+// 		console.log(this.name, this.clas)
+// 	};
+// }
+// class HeadBoy extends Student {
+
+// }
+
+// const s1 = new HeadBoy("sachin", 12)
+// s1.data()
+
+//4.How to find duplicate elements in a given array?
+
+// const arr = [1, 1, 1, 2, 3, 4, 4, 5, 5, 6, 5]
+
+// const arr1= arr.filter((item, index) => arr.indexOf
+// 	(item) !== index)
+
+// const arr2= arr1.filter((item, index) => arr1.indexOf
+// 		(item) !== index)
+// console.log(arr2)
+//5.How to find the count of duplicates in an array?
+
+
